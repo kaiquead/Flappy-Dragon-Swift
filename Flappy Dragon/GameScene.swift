e5ae352066e145636363ab104385333f901490cb
Kaique Damacena <kaique.damacena@xds.com.br>
2020-09-16 22:16:56 -0300

    Added animation in floor (now its moving)

--- Flappy Dragon/GameScene.swift
+++ Flappy Dragon/GameScene.swift
@@ -15,12 +15,14 @@ class GameScene: SKScene {
     var intro: SKSpriteNode!
     var player: SKSpriteNode!
     var gameArea: CGFloat = 410.0
+    var velocity: Double = 100.0
     
     override func didMove(to view: SKView) {
         addBackground()
         addFloor()
         addIntro()
         addPlayer()
+        moveFloor()
     }
     
     func addBackground() {
@@ -48,7 +50,26 @@ class GameScene: SKScene {
         player = SKSpriteNode(imageNamed: "player1")
         player.zPosition = 4
         player.position = CGPoint(x: 60, y: size.height - gameArea/2)
+        
+        var playerTextures = [SKTexture]()
+        for i in 1...4 {
+            playerTextures.append(SKTexture(imageNamed: "player\(i)"))
+        }
+        let animateAction = SKAction.animate(with: playerTextures, timePerFrame: 0.09)
+        let repeatAction = SKAction.repeatForever(animateAction)
+        player.run(repeatAction)
+        
+        
         addChild(player)
+    }
+    
+    func moveFloor() {
+        let duration = Double(floor.size.width/2)/velocity
+        let moveFloorAction = SKAction.moveBy(x: -floor.size.width/2, y: 0, duration: duration)
+        let resetXAction = SKAction.moveBy(x: floor.size.width/2, y: 0, duration: 0)
+        let sequenceAction = SKAction.sequence([moveFloorAction, resetXAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        floor.run(repeatAction)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
